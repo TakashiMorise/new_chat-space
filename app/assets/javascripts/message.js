999623bd46b783b9bd93449caea12613647c3e82
森瀬 隆司 <55taac.tac@gmail.com>
2019-01-13 07:45:52 +0900

    completed the js file

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,29 @@
 $(function(){
+  function buildHTML(message){
+    var imagePresent = "";
+    if (message.image) {
+      imagePresent = `<img class="lower-message__image" src=${message.image}>`
+    }
+    var html = `<div class="chat-main__bulletin-board__messages__message">
+                  <div class="chat-main__bulletin-board__messages__message-name">
+                    ${message.name}
+                  </div>
+                  <div class="chat-main__bulletin-board__messages__message-date">
+                    ${message.created_at}
+                  </div>
+                  <div class="chat-main__bulletin-board__messages__message-text">
+                    ${message.content}
+                    ${imagePresent}
+                  </div>
+                </div>`
+
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this);
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    console.log(url)
     $.ajax({
       url: url,
       type: "POST",
@@ -12,6 +31,19 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.chat-main__bulletin-board__messages').append(html)
+      $('#new_message')[0].reset();
+      console.log("森瀬隆司")
+      $('.chat-main__bulletin-board').animate({scrollTop: $('.chat-main__bulletin-board')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(() => {
+      $(".chat-main__footer__form__submit").removeAttr("disabled");
     });
   });
 });
