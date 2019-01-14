67213fd8ba1b52fe55393afc67021a24d413b977
森瀬 隆司 <55taac.tac@gmail.com>
2019-01-14 13:36:08 +0900

    create js-file

---
+++ app/assets/javascripts/user-search.js
@@ -0,0 +1,61 @@
+$(function(){
+  function displayUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    return html;
+  }
+  function noUserName(message) {
+    var display = `<div class="chat-group-users clearfix">
+                     <p class="chat-group-user__name">${message}</p>
+                   </div>`
+    return display;
+  }
+  function removeUser(userId, name) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value=${userId}>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    return html
+  }
+  $('#user-search-field').on('keyup', function(){
+    var input = $(this).val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input, },
+      dataType: 'json'
+    })
+    .done(function(data){
+      console.log('森瀬隆司')
+     $('#chat-group-users').empty();
+     if (data.length !== 0) {
+      data.forEach(function(user){
+        var html = displayUser(user);
+        $('#chat-group-users').append(html);
+      })
+     }
+     else {
+      var display = noUserName("一致するユーザーはいません");
+      $('#chat-group-users').append(display);
+     }
+    })
+    .fail(function(){
+      alert('通信に失敗しました');
+    });
+  });
+  $('#chat-group-users').on('click', '.user-search-add', function(){
+    var userId = $(this).attr('data-user-id');
+    var name = $(this).attr('data-user-name');
+    console.log(userId);
+    console.log(name);
+    $(this).parent().remove();
+    var html = removeUser(userId, name);
+    $('#chat-group-users').append(html);
+  });
+  $('#chat-group-users').on('click', '.user-search-remove', function(){
+    $(this).parent().remove();
+  });
+});
