633909989eb4c95e49e127f991acdd8522bdc8d5
森瀬 隆司 <55taac.tac@gmail.com>
2019-01-19 14:13:07 +0900

    create automatic_update.js

---
+++ app/assets/javascripts/automatic_update.js
@@ -0,0 +1,44 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message){
+    var imagePresent = "";
+    if (message.image) {
+      imagePresent = `<img class="lower-message__image" src=${message.image}>`
+    }
+    var html = `<div class="chat-main__bulletin-board__messages__message" data-message-id="${message.message_id}">
+                  <div class="chat-main__bulletin-board__messages__message-name">
+                    ${message.name}
+                  </div>
+                  <div class="chat-main__bulletin-board__messages__message-date">
+                    ${message.created_at}
+                  </div>
+                  <div class="chat-main__bulletin-board__messages__message-text">
+                    ${message.content}
+                    ${imagePresent}
+                  </div>
+                </div>`
+    return html;
+  }
+  function auto_update() {
+    if ($('.chat-main__bulletin-board__messages__message')[0]){
+      var message_id = $('.chat-main__bulletin-board__messages__message:last').data('message-id');
+    } else {
+      var message_id = 0;
+    }
+    $.ajax({
+      url: window.location.pathname,
+      type: 'GET',
+      data: { id: message_id },
+      dataType: 'json'
+    })
+    .done(function(data) {
+      data.forEach(function(data){
+        var html = buildHTML(data);
+        $('.chat-main__bulletin-board__messages').append(html);
+        $('.chat-main__bulletin-board').animate({scrollTop: $('.chat-main__bulletin-board')[0].scrollHeight}, 'fast');
+      });
+    });
+  }
+  if (location.pathname.match(/\/groups\/\d+\/messages/)){
+    setInterval(auto_update, 5000);
+  }
+});
